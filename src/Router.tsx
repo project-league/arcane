c360552f88cc213b7ff5db46d55b9138281ee157
Weronika Kalinowska <wkalinowska406@gmail.com>
2025-09-03 21:16:13 +0200

    Fix with hash

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,42 +1,17 @@
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  createHashRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-  Routes,
-} from 'react-router-dom';
+import { HashRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import { Final } from './pages/Final';
+import { FirstPage } from './pages/First';
+import { SecondPage } from './pages/Second';
 import { Start } from './pages/Start';
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/arcane/',
-//     element: <Final />,
-//   },
-//   {
-//     path: '/arcane/start/',
-//     element: <Start />,
-//   },
-// ]);
-
-// const router = createHashRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<Final />}>
-//       {/* <Route path="/arcane/" element={<Final />} /> */}
-//       <Route path="/start/" element={<Start />} />
-//     </Route>
-//   )
-// );
+import { Tickets } from './pages/Tickets';
 
 export function Router() {
   return (
-    <BrowserRouter basename="/arcane">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Final />} />
-        <Route path="/start/" element={<Start />} />
+        <Route path="/start" element={<Start />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
