15d2d5444b618678c524a2dbe025738902b60e28
Weronika Kalinowska <wkalinowska406@gmail.com>
2025-09-03 21:10:52 +0200

    Fix maybe

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,42 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  createHashRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Routes,
+} from 'react-router-dom';
 import { Final } from './pages/Final';
 import { Start } from './pages/Start';
 
-const router = createBrowserRouter([
-  {
-    path: '/arcane/',
-    element: <Final />,
-  },
-  {
-    path: '/arcane/start/',
-    element: <Start />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/arcane/',
+//     element: <Final />,
+//   },
+//   {
+//     path: '/arcane/start/',
+//     element: <Start />,
+//   },
+// ]);
+
+// const router = createHashRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Final />}>
+//       {/* <Route path="/arcane/" element={<Final />} /> */}
+//       <Route path="/start/" element={<Start />} />
+//     </Route>
+//   )
+// );
 
 export function Router() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter basename="/arcane">
+      <Routes>
+        <Route path="/" element={<Final />} />
+        <Route path="/start/" element={<Start />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
